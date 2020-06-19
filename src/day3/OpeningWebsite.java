d7b027057d0b538138cc753b07ff4ba57be9e415
daulet2030 <daulet2030@gmail.com>
2020-06-19 20:27:34 +0600

    opening the website

--- src/day3/OpeningWebsite.java
+++ src/day3/OpeningWebsite.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpeningWebsite {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
+        driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
+        Thread.sleep(3000); // wait for 3 seconds
+        driver.quit();
     }
 }
