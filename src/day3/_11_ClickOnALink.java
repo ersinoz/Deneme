2a2ecb1b283a05c3d227cc36ef657d6707de71c1
daulet2030 <daulet2030@gmail.com>
2020-06-19 22:07:44 +0600

    clicking on a link

---
+++ src/day3/_11_ClickOnALink.java
@@ -0,0 +1,19 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class _11_ClickOnALink {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); // this will open the chrome browser
+        driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
+        WebElement link = driver.findElement(By.linkText("Check Out"));
+        link.click();
+//        driver.quit();
+    }
+}
