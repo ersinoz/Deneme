d21042b4eb2a38d90ddf182ce473dfd20b85b054
daulet2030 <daulet2030@gmail.com>
2020-06-19 21:14:46 +0600

    finding by class name

--- src/day3/_04_FindingByClassName.java
+++ src/day3/_04_FindingByClassName.java
@@ -10,7 +10,8 @@ public class _04_FindingByClassName {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
         driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
-
+        WebElement element = driver.findElement(By.className("editor_collections")); // will return first found element only
+        System.out.println(element.getText());
         driver.quit();
     }
 }
