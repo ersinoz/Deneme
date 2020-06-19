2fb32c1bbbf869d06e789754418fa547a16a86ea
daulet2030 <daulet2030@gmail.com>
2020-06-19 20:07:56 +0600

    day3

---
+++ src/day3/OpeningWebsite.java
@@ -0,0 +1,11 @@
+package day3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpeningWebsite {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    }
+}
