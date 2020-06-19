571350803d4d70047193d6bf2d5a9d8fa746668c
daulet2030 <daulet2030@gmail.com>
2020-06-19 21:19:48 +0600

    finding by link text

---
+++ src/day3/_05_FindingByLinkText.java
@@ -0,0 +1,17 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _05_FindingByLinkText {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); // this will open the chrome browser
+        driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
+        WebElement checkOutLink = driver.findElement(By.linkText("Check Out"));
+        System.out.println(checkOutLink.getAttribute("data-pageid"));
+        driver.quit();
+    }
+}
