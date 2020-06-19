d510dbe16dbdf27497b281b1dee46c639d601084
daulet2030 <daulet2030@gmail.com>
2020-06-19 21:26:01 +0600

    finding by partial link text

--- src/day3/_06_FindingByLinkPartialText.java
+++ src/day3/_06_FindingByLinkPartialText.java
@@ -10,8 +10,8 @@ public class _06_FindingByLinkPartialText {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
         driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
-        WebElement teaLink = driver.findElement(By.partialLinkText("Tea"));
-        System.out.println(teaLink.getAttribute("href"));
+        WebElement passionLink = driver.findElement(By.partialLinkText("Passion"));
+        System.out.println(passionLink.getAttribute("href"));
         driver.quit();
     }
 }
