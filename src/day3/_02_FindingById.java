d37505af48171ad19745c26eca39cbd9cec942f1
daulet2030 <daulet2030@gmail.com>
2020-06-19 21:09:07 +0600

    refactoring names

--- src/day3/_02_FindingById.java
+++ src/day3/_02_FindingById.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FindingById {
+public class _02_FindingById {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
--- src/day3/_03_FindingByIdNotFoundException.java
+++ src/day3/_03_FindingByIdNotFoundException.java
@@ -1,14 +1,17 @@
 package day3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OpeningWebsite {
+public class _03_FindingByIdNotFoundException {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
         driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
-        Thread.sleep(3000); // wait for 3 seconds
-        driver.quit();
+        WebElement e = driver.findElement(By.id("none-existing-element-id")); //throws exception
+        System.out.println(e.getText()); //because of the exception this code was not executed
+        driver.quit();//because of the exception this code was not executed
     }
 }
