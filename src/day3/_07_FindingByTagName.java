8e8705e5dfce2bb98d3300b616e988f7408f26d9
daulet2030 <daulet2030@gmail.com>
2020-06-19 21:29:37 +0600

    finding by tag name

---
+++ src/day3/_07_FindingByTagName.java
@@ -0,0 +1,17 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _07_FindingByTagName {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); // this will open the chrome browser
+        driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
+        WebElement h1 = driver.findElement(By.tagName("p"));
+        System.out.println(h1.getText());
+        driver.quit();
+    }
+}
