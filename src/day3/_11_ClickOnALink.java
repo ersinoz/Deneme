6ddd90923408e89961553a6bcd79009f4a708a7c
daulet2030 <daulet2030@gmail.com>
2020-06-19 22:09:27 +0600

    clicking on a link

--- src/day3/_11_ClickOnALink.java
+++ src/day3/_11_ClickOnALink.java
@@ -12,8 +12,7 @@ public class _11_ClickOnALink {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
         driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
-        WebElement link = driver.findElement(By.linkText("Check Out"));
-        link.click();
+        driver.findElement(By.linkText("Check Out")).click();
 //        driver.quit();
     }
 }
