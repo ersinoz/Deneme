a03ee0f88aafb7872d3a34b7558827c547221819
daulet2030 <daulet2030@gmail.com>
2020-06-19 21:46:13 +0600

    finding elements

---
+++ src/day3/_09_FindingElements.java
@@ -0,0 +1,22 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class _09_FindingElements {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); // this will open the chrome browser
+        driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
+        List<WebElement> liElements = driver.findElements(By.tagName("li"));
+        for (int i = 0; i < liElements.size(); i++) {
+            WebElement element = liElements.get(i);
+            System.out.println(element.getText());
+        }
+        driver.quit();
+    }
+}
