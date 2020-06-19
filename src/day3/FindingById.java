15faad6f6db9f2f77301e4e78ae87a4feea109e4
daulet2030 <daulet2030@gmail.com>
2020-06-19 20:47:22 +0600

    finding by id

--- src/day3/FindingById.java
+++ src/day3/FindingById.java
@@ -10,8 +10,8 @@ public class FindingById {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
         driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
-        WebElement element = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914886"));
-        System.out.println(element.getText());
+        WebElement e = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914886"));
+        System.out.println(e.getText());
         driver.quit();
     }
 }
