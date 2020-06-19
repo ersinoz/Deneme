d0cfc4c7515a9f7cde89c81386b86ec091d6ef5d
daulet2030 <daulet2030@gmail.com>
2020-06-19 20:40:28 +0600

    finding by id

---
+++ src/day3/FindingById.java
@@ -0,0 +1,17 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindingById {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); // this will open the chrome browser
+        driver.get("http://www.practiceselenium.com");  // this will open the website and wait until full load
+        WebElement element = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914886"));
+        System.out.println(element.getText());
+        driver.quit();
+    }
+}
