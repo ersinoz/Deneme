d70bf99e86067a459aba76afe5b0dd82dd5ada75
daulet2030 <daulet2030@gmail.com>
2020-06-22 20:35:09 +0600

    day4 select by id

--- src/day4/_02_SelectingById.java
+++ src/day4/_02_SelectingById.java
@@ -1,11 +1,11 @@
 package day4;
 
+import org.openqa.selenium.By;
 import utils.BaseDriver;
 
 public class _02_SelectingById extends BaseDriver {
     public static void main(String[] args) throws InterruptedException {
         driver.get("https://formsmarts.com/form/yu?mode=h5");  // this will open the website and wait until full load
-        Thread.sleep(3000); // wait for 3 seconds
-        driver.quit();
+        driver.findElement(By.cssSelector("#u_Uj7_4586_1")).click();
     }
 }
