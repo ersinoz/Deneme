d16ef11ac591cd0837df96cc9f45a71d8d2e4200
daulet2030 <daulet2030@gmail.com>
2020-06-22 22:09:15 +0600

    day4 selecting by general sibling

---
+++ src/day4/_07_SelectingByGeneralSibling.java
@@ -0,0 +1,20 @@
+package day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+import java.util.List;
+
+public class _07_SelectingByGeneralSibling extends BaseDriver {
+    public static void main(String[] args) throws InterruptedException {
+        driver.get("https://formsmarts.com/form/yu?mode=h5");  // this will open the website and wait until full load
+
+       // using general sibling selector print all options of "How often do you use XYZ?" question
+        List<WebElement> elements = driver.findElements(By.cssSelector("#u_Uj7_89585_lbl ~ label"));
+        for(WebElement element : elements) {
+            System.out.println(element.getText());
+        }
+
+    }
+}
