a32607234a4ef264696801e3c8349133d6304bf4
daulet2030 <daulet2030@gmail.com>
2020-06-22 22:39:58 +0600

    day4 task

---
+++ src/day4/_10_CountriesTask.java
@@ -0,0 +1,16 @@
+package day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+import java.util.List;
+
+public class _10_CountriesTask extends BaseDriver {
+    public static void main(String[] args) throws InterruptedException {
+        driver.get("https://formsmarts.com/form/yu?mode=h5");  // this will open the website and wait until full load
+
+        // find me all countries where country code doesn't start with the same letter as country name
+
+    }
+}
