dd96baad586c0b2f2b1e8c50aa2923e6bc7500e4
daulet2030 <daulet2030@gmail.com>
2020-06-22 20:54:26 +0600

    day4 selecting by attribute

---
+++ src/day4/_05_SelectingByAttribute.java
@@ -0,0 +1,10 @@
+package day4;
+
+import org.openqa.selenium.By;
+import utils.BaseDriver;
+
+public class _05_SelectingByAttribute extends BaseDriver {
+    public static void main(String[] args) throws InterruptedException {
+        driver.get("https://formsmarts.com/form/yu?mode=h5");  // this will open the website and wait until full load
+    }
+}
