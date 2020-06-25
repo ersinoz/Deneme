d835ab567208d7deacd0fcc27a9367744e48a99a
daulet2030 <daulet2030@gmail.com>
2020-06-25 21:06:46 +0600

    day 6 refactor

--- src/day6/_03_AddToCart.java
+++ src/day6/_03_AddToCart.java
@@ -1,39 +1,15 @@
 package day6;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import utils.BaseDriver;
 
-import java.util.ArrayList;
-
-public class _01_LoginFunctionTest extends BaseDriver {
+public class _03_AddToCart extends BaseDriver {
     public static void main(String[] args) {
         driver.get("https://www.saucedemo.com/");
-        ArrayList<String> usernames = new ArrayList<>();
-        usernames.add("standard_user");
-        usernames.add("locked_out_user");
-        usernames.add("problem_user");
-        usernames.add("performance_glitch_user");
-        usernames.add("non_existing_user");
-        String password = "secret_sauce";
-
-        ArrayList<String>  expectedErrorMessages =  new ArrayList<>();
-        expectedErrorMessages.add("success");
-        expectedErrorMessages.add("Epic sadface: Sorry, this user has been locked out.");
-        expectedErrorMessages.add("success");
-        expectedErrorMessages.add("success");
-        expectedErrorMessages.add("Epic sadface: Username and password do not match any user in this service");
-
-        for (int i = 0; i < usernames.size(); i++) {
-            String loginResult = login(usernames.get(i), password);
-            if(loginResult.equals("success")) {
-                driver.navigate().back(); // TODO: create log out function here
-            }
-            Assert.assertEquals(expectedErrorMessages.get(i), loginResult);
-        }
-        driver.quit();
+        String result = login("locked_out_user", "secret_sauce");
+        System.out.println(result);
     }
 
     // login function
@@ -59,4 +35,14 @@ public class _01_LoginFunctionTest extends BaseDriver {
             }
         }
     }
+
+    // add to cart function
+    // get the number of items in the cart
+    // remove item from the cart
+    // get the list of available items
+    // get the list of items in the cart
+    // go to checkout page
+    // check prices in the checkout page
+    // check tax in the checkout page
+    // check thank you message
 }
