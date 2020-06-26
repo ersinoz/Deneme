e52eb66a14e2fdafadeb42a25687c2c6c44090b2
Dauke <daulet2030@gmail.com>
2020-06-26 22:38:31 +0600

    day 7 prompt alert test

--- src/day7/_05_PromptAlertTest1.java
+++ src/day7/_05_PromptAlertTest1.java
@@ -9,6 +9,18 @@ public class _05_PromptAlertTest1 extends BaseDriver {
         driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html");
         driver.findElement(By.cssSelector("button[onclick='myPromptFunction()']")).click();
 
+        // create a test string
+        String testString = "Hello!";
+        // switch to alert and send keys of test string to it
+        driver.switchTo().alert().sendKeys(testString);
+        // press accept on alert
+        driver.switchTo().alert().accept();
+
+        // find the result text on the website
+        String actualText =driver.findElement(By.xpath("//p[@id='prompt-demo']")).getText();
+        // confirm that the result test contains test string
+        boolean contains = actualText.contains(testString);
+        Assert.assertTrue("Actual text should contain the test string!", contains);
     }
 
 }
