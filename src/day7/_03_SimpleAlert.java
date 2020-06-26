85474cc5e1f0d9dc8e0056cf4ecd3bb7a4c0d795
Dauke <daulet2030@gmail.com>
2020-06-26 22:09:40 +0600

    day 7 simple alert

---
+++ src/day7/_03_SimpleAlert.java
@@ -0,0 +1,17 @@
+package day7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import utils.BaseDriver;
+
+public class _03_SimpleAlert extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html");
+        //driver.findElement(By.xpath("//button[@onclick='myAlertFunction()']")).click();
+        driver.findElement(By.cssSelector("button[onclick='myAlertFunction()']")).click();
+        String alertText = driver.switchTo().alert().getText();
+        System.out.println(alertText);
+        driver.switchTo().alert().accept();
+    }
+
+}
