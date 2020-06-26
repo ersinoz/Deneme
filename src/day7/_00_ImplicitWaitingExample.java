45c791ea94b1d10aadef9d564031a87294926fde
Dauke <daulet2030@gmail.com>
2020-06-26 21:36:02 +0600

    day 7 implicit wait

--- src/day7/_00_ImplicitWaitingExample.java
+++ src/day7/_00_ImplicitWaitingExample.java
@@ -10,6 +10,7 @@ public class _00_ImplicitWaitingExample extends BaseDriver {
         driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         // without this timeout, the below code will throw exception right away
+        // if the below element is found after 3 seconds, this code will continue right away
         System.out.println(driver.findElement(By.id("name-and-slogan2")).getText());
     }
 
--- src/utils/BaseDriver.java
+++ src/utils/BaseDriver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BaseDriver {
     public static WebDriver driver;
     static {
-        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\cmd_soft\\selenium\\driver\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 }
