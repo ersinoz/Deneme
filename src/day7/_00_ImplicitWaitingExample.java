7fb909475a3f0a94ee5885048ff6fe71e23b553f
Dauke <daulet2030@gmail.com>
2020-06-26 21:32:06 +0600

    day 7 implicit wait timeout example

---
+++ src/day7/_00_ImplicitWaitingExample.java
@@ -0,0 +1,16 @@
+package day7;
+
+import org.openqa.selenium.By;
+import utils.BaseDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class _00_ImplicitWaitingExample extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // without this timeout, the below code will throw exception right away
+        System.out.println(driver.findElement(By.id("name-and-slogan2")).getText());
+    }
+
+}
