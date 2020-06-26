8ef6fcb1bbf179cb538a95efefe2a284207702d2
Dauke <daulet2030@gmail.com>
2020-06-26 21:17:05 +0600

    day 7

---
+++ src/day7/_00_.java
@@ -0,0 +1,11 @@
+package day7;
+
+import utils.BaseDriver;
+
+public class _00_ extends BaseDriver {
+    public static void main(String[] args) {
+
+        driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html");
+    }
+
+}
