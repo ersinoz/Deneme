cbaca99a9bfe9e0891aebc360059437c1dae1e6a
daulet2030 <daulet2030@gmail.com>
2020-06-29 20:28:00 +0600

    day 8 will throw and exception because no such element present

---
+++ src/day8/_02_AddedRemovedElements.java
@@ -0,0 +1,16 @@
+package day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+public class _02_AddedRemovedElements extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement button = driver.findElement(By.cssSelector("button[onclick=\"addElement()\"]"));
+        WebElement element = driver.findElement(By.cssSelector("button[class=\"added-manually\"]"));
+        System.out.println("element is displayed: " + element.isDisplayed());
+    }
+
+}
