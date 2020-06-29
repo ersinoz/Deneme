83d6b653df198c829046ba924384358cec72d2c6
daulet2030 <daulet2030@gmail.com>
2020-06-29 20:28:32 +0600

    day 8 will not throw exception

--- src/day8/_02_AddedRemovedElements.java
+++ src/day8/_02_AddedRemovedElements.java
@@ -9,6 +9,7 @@ public class _02_AddedRemovedElements extends BaseDriver {
     public static void main(String[] args) {
         driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
         WebElement button = driver.findElement(By.cssSelector("button[onclick=\"addElement()\"]"));
+        button.click();
         WebElement element = driver.findElement(By.cssSelector("button[class=\"added-manually\"]"));
         System.out.println("element is displayed: " + element.isDisplayed());
     }
