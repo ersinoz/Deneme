303a459eb39bf192ca43adad582ea3a6009ad0d5
daulet2030 <daulet2030@gmail.com>
2020-06-29 22:06:28 +0600

    day 8 select insurance solution

--- src/day8/_09_SelectInsuranceCategory.java
+++ src/day8/_09_SelectInsuranceCategory.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 import utils.BaseDriver;
 
-public class _09_SelectInsuranceCategory extends BaseDriver {
-    public static void main(String[] args) throws InterruptedException {
-        driver.get("https://www.iaai.com/");
-        driver.findElement(By.id("js-keyword-filter")).click();
+import java.util.concurrent.TimeUnit;
 
+public class _09_SelectInsuranceCategory extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("https://www.iaai.com/");
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.id("js-keyword-filter")).click();
         Select categories = new Select(driver.findElement(By.id("VehiclesType")));
         categories.selectByIndex(0);
 //        driver.quit();
