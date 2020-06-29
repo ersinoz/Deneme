6be5cbb51f908fc4c90f38eb004be59a081ef905
daulet2030 <daulet2030@gmail.com>
2020-06-29 22:29:34 +0600

    day 8 calculator making a method

--- src/day8/_10_CalculatorTest.java
+++ src/day8/_10_CalculatorTest.java
@@ -2,23 +2,43 @@ package day8;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utils.BaseDriver;
 
 public class _10_CalculatorTest extends BaseDriver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         driver.get("https://testsheepnz.github.io/BasicCalculator.html");
-        String answer = calculate("1", "1", "Subtract");
-        Assert.assertEquals("0", answer);
+        Select operations = new Select(driver.findElement(By.id("selectOperationDropdown")));
+        for (WebElement operation: operations.getOptions()) {
+            String answer = calculate("1", "1", operation.getText());
+            if(operation.getText().equals("Add")) {
+                Assert.assertEquals("2", answer);
+            } else if(operation.getText().equals("Subtract")) {
+                Assert.assertEquals("0", answer);
+            } else if(operation.getText().equals("Multiply")) {
+                Assert.assertEquals("1", answer);
+            } else if(operation.getText().equals("Divide")) {
+                Assert.assertEquals("1", answer);
+            } else if(operation.getText().equals("Concatenate")) {
+                Assert.assertEquals("11", answer);
+            }
+
+        }
         driver.quit();
     }
 
-    private static String calculate(String operand1, String operand2, String operation) {
-        driver.findElement(By.id("number1Field")).sendKeys(operand1);
-        driver.findElement(By.id("number2Field")).sendKeys(operand2);
+    private static String calculate(String operand1, String operand2, String operation) throws InterruptedException {
+        WebElement number1Field = driver.findElement(By.id("number1Field"));
+        number1Field.clear();
+        number1Field.sendKeys(operand1);
+        WebElement number2Field = driver.findElement(By.id("number2Field"));
+        number2Field.clear();
+        number2Field.sendKeys(operand2);
         Select operations = new Select(driver.findElement(By.id("selectOperationDropdown")));
         operations.selectByVisibleText(operation);
         driver.findElement(By.id("calculateButton")).click();
+        Thread.sleep(1000); // should be done explicit wait
         return driver.findElement(By.id("numberAnswerField")).getAttribute("value");
 
     }
