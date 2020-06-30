7c193ecf80ffd6b15fcfdb0d0235725df0274efa
daulet2030 <daulet2030@gmail.com>
2020-06-30 22:54:46 +0600

    day 8 Actions class hover over test fix

--- src/day9/_07_ActionHoverOverTest.java
+++ src/day9/_07_ActionHoverOverTest.java
@@ -3,6 +3,7 @@ package day9;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.Color;
 import utils.BaseDriver;
@@ -12,22 +13,16 @@ import java.util.List;
 public class _07_ActionHoverOverTest extends BaseDriver {
     public static void main(String[] args) throws InterruptedException {
         driver.get("http://demo.guru99.com/test/drag_drop.html");
-        WebElement bankButton = driver.findElement(By.id("credit2"));
-        Actions actions = (new Actions(driver)).moveToElement(bankButton);
-        Thread.sleep(2000);
-        // color before hover over
-        String rgbaColor = bankButton.getCssValue("color");
-        System.out.println(Color.fromString(rgbaColor).asHex());
+        WebElement e = driver.findElement(By.cssSelector("li.ui-draggable:first-child > a"));
+        Actions actionsBuilder = new Actions(driver);
+        Action action = actionsBuilder.moveToElement(e).click().build();
+        System.out.println(Color.fromString(e.getCssValue("color")).asHex());
+        action.perform();
+        System.out.println(Color.fromString(e.getCssValue("color")).asHex());
 
-        // hover over bankButton action
-        actions.perform();
-        Thread.sleep(2000);
-
-        // color after hover over
-        bankButton = driver.findElement(By.id("credit2"));
-        rgbaColor = bankButton.getCssValue("color");
-        System.out.println(Color.fromString(rgbaColor).asHex());
-
+        // be careful, the css property value should be applied to the web element that you are inspecting
+        // if you select #credit2 element which is actually a li element, the style is not applied to it
+        // on this website the style of color change on hover is applied to a element, not li element
     }
 
 }
