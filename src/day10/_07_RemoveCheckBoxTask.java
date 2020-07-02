79c40c7e7901d3d67cb78722e289d1f59e3bd41e
daulet2030 <daulet2030@gmail.com>
2020-07-02 22:45:41 +0600

    day 10 checkbox task solution

--- src/day10/_07_RemoveCheckBoxTask.java
+++ src/day10/_07_RemoveCheckBoxTask.java
@@ -1,6 +1,8 @@
 package day10;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.BaseDriver;
@@ -8,12 +10,28 @@ import utils.BaseDriver;
 public class _07_RemoveCheckBoxTask extends BaseDriver {
     public static void main(String[] args)  {
         driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        // assert that checkbox exists
-        // click on the remove button
-        // assert that check box removed
-        // click on the add button
-        // assert that check box re-appeared
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
+        // assert that checkbox exists
+        driver.findElement(By.cssSelector("input[type=checkbox]"));
+
+        // click on the remove button
+        driver.findElement(By.cssSelector("#checkbox-example>button")).click();
+
+        // assert that check box removed, this test is enough
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("input[type=checkbox]")));
+        // just additional test
+        WebElement goneText = driver.findElement(By.cssSelector("#checkbox-example>p"));
+        Assert.assertEquals("It's gone!", goneText.getText());
+
+        // click on the add button
+        driver.findElement(By.cssSelector("[onclick=\"swapCheckbox()\"]")).click();
+
+        // assert that check box re-appeared, this test is enough
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[type=checkbox]")));
+        // just additional test
+        WebElement appearText = driver.findElement(By.cssSelector("#checkbox-example>p"));
+        Assert.assertEquals("It's back!", appearText.getText());
     }
 
 }
