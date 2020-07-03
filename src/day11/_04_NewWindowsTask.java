fcdda3bb662af6e25ac04cbc8d7bbeaf95f2cd4f
daulet2030 <daulet2030@gmail.com>
2020-07-03 21:05:00 +0600

    day 11 new window task

---
+++ src/day11/_04_NewWindowsTask.java
@@ -0,0 +1,18 @@
+package day11;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class _04_NewWindowsTask extends BaseDriver {
+    public static void main(String[] args) throws InterruptedException {
+        driver.get("https://demoqa.com/browser-windows");
+        // click on the new window button, and verify that in new windows you have text: "This is a sample page"
+
+
+    }
+
+}
