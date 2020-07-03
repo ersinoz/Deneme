75ec88d9c1b2d2de98631e9acd6fdebfd804df93
daulet2030 <daulet2030@gmail.com>
2020-07-03 21:17:53 +0600

    day 11 new window task solution

--- src/day11/_04_NewWindowsTask.java
+++ src/day11/_04_NewWindowsTask.java
@@ -1,6 +1,8 @@
 package day11;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.BaseDriver;
 
@@ -11,8 +13,18 @@ public class _04_NewWindowsTask extends BaseDriver {
     public static void main(String[] args) throws InterruptedException {
         driver.get("https://demoqa.com/browser-windows");
         // click on the new window button, and verify that in new windows you have text: "This is a sample page"
+        String mainWindow = driver.getWindowHandle();
 
+        // click on the new window button
+        driver.findElement(By.id("tabButton")).click();
 
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String handle: windowHandles) {
+            if(!handle.equals(mainWindow)) {
+                driver.switchTo().window(handle);
+                WebElement sampleHeading = driver.findElement(By.id("sampleHeading"));
+                Assert.assertEquals("This is a sample page", sampleHeading.getText());
+            }
+        }
     }
-
 }
