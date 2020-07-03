410bc650c059f95bb08a42b84e63438226d946ab
daulet2030 <daulet2030@gmail.com>
2020-07-03 21:26:32 +0600

    day 11 add to cart task

--- src/day11/_05_AddToCartWindowTask.java
+++ src/day11/_05_AddToCartWindowTask.java
@@ -11,10 +11,12 @@ public class _05_AddToCartWindowTask extends BaseDriver {
     public static void main(String[] args) {
         driver.get("https://www.snapdeal.com/");
         // click on any product
-        //store the product name
-        //click on add to cart
-        //close the windows
-        //click on the cart|
-        //verify that cart contains you product
+        // switch to new window that was opened
+        // store the product name
+        // click on add to cart
+        // close the window
+        // switch back to main window
+        // click on the cart
+        // verify that cart contains you product
     }
 }
