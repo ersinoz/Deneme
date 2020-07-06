bab708524f77bc9f29266df5d8991d9814eb0256
daulet2030 <daulet2030@gmail.com>
2020-07-06 22:24:33 +0600

    day 12 scroll https://romainpenchenat.com/

---
+++ src/day12/_10_Romainpenchenat.java
@@ -0,0 +1,19 @@
+package day12;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+public class _10_Romainpenchenat extends BaseDriver {
+
+    public static void main(String[] args) throws InterruptedException {
+        driver.get("https://romainpenchenat.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        for (int i = 0; i < 2000 ; i++) {
+            js.executeScript("window.scrollBy(0,10)");
+        }
+        System.out.println("Done");
+//        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    }
+}
