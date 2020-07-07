bb82148b9e97886e65b2183e0f7a41e9673062c5
daulet2030 <daulet2030@gmail.com>
2020-07-07 22:01:54 +0600

    day 13 task2

---
+++ src/day13/_06_ShoppingCartPracticeTask2.java
@@ -0,0 +1,24 @@
+package day13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+import java.util.List;
+import java.util.Random;
+
+public class _06_ShoppingCartPracticeTask2 extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("http://practice.automationtesting.in/shop/");
+        // add random number of items into the basket
+        List<WebElement> items = driver.findElements(By.cssSelector("a[class*='add_to_cart_button']"));
+        Integer numberOfItems = new Random().nextInt(items.size()) + 1;
+        System.out.println("numberOfItems: "+numberOfItems);
+        for (int i = 0; i < numberOfItems; i++) {
+            items.get(i).click();
+            items = driver.findElements(By.cssSelector("a[class*='add_to_cart_button']"));
+        }
+
+        // go to checkout page and fill out the form
+    }
+}
