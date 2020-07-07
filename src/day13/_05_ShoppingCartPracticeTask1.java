c4fa1b3f16e277127c70093e2cdcb18d2a804208
daulet2030 <daulet2030@gmail.com>
2020-07-07 21:20:43 +0600

    day 13 task1 add to cart

---
+++ src/day13/_05_ShoppingCartPracticeTask1.java
@@ -0,0 +1,12 @@
+package day13;
+
+import org.openqa.selenium.By;
+import utils.BaseDriver;
+
+public class _05_ShoppingCartPracticeTask1 extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("http://practice.automationtesting.in/shop/");
+        // add random number of items into the basket
+
+    }
+}
