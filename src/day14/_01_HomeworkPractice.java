6e40dcc7f15aab603195aecd3f99d4f207c5114d
daulet2030 <daulet2030@gmail.com>
2020-07-09 21:38:38 +0600

    day 14 homework task solution part 1

--- src/day14/_01_HomeworkPractice.java
+++ src/day14/_01_HomeworkPractice.java
@@ -1,13 +1,43 @@
 package day14;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.BaseDriver;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
+import java.util.List;
+import java.util.Random;
 
 public class _01_HomeworkPractice extends BaseDriver {
     public static void main(String[] args) {
         driver.get("https://pwa-woo.wpmobilepack.com/#/category/88");
+        WebDriverWait wait = new WebDriverWait(driver, 5); // the timeout of 5 seconds
+
+        goToRandomItem(wait);
+        getNameAndAddToCart(wait);
+
+        driver.navigate().back();
+
+        goToRandomItem(wait);
+        getNameAndAddToCart(wait);
+
+    }
+
+    private static String getNameAndAddToCart(WebDriverWait wait) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".pusher .header")));
+        String itemName = driver.findElement(By.cssSelector(".pusher .header")).getText();
+        System.out.println("itemName: " + itemName);
+        driver.findElement(By.cssSelector(".card button[role=button]")).click();
+        return itemName;
+    }
+
+    private static void goToRandomItem(WebDriverWait wait) {
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".infinite-scroll-component button"), 0));
+        List<WebElement> items = driver.findElements(By.cssSelector(".infinite-scroll-component button"));
+        System.out.println("items size: "+ items.size());
+        int randomItem = new Random().nextInt(items.size());
+        items.get(randomItem).click();
     }
 }
 
