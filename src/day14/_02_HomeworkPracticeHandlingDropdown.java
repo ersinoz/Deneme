a718e75ed11071365b3a8ee3c89e425e568eb9e5
daulet2030 <daulet2030@gmail.com>
2020-07-09 21:47:39 +0600

    day 14 homework task solution part 2 handling dropboxes

---
+++ src/day14/_02_HomeworkPracticeHandlingDropdown.java
@@ -0,0 +1,52 @@
+package day14;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.BaseDriver;
+
+import java.util.List;
+import java.util.Random;
+
+public class _02_HomeworkPracticeHandlingDropdown extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("https://pwa-woo.wpmobilepack.com/#/product/1297");
+        WebDriverWait wait = new WebDriverWait(driver, 5); // the timeout of 5 seconds
+        getNameAndAddToCart(wait);
+    }
+
+    private static String getNameAndAddToCart(WebDriverWait wait) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".pusher .header")));
+        String itemName = driver.findElement(By.cssSelector(".pusher .header")).getText();
+        System.out.println("itemName: " + itemName);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[role=\"listbox\"]")));
+        List<WebElement> dropDownBoxes = driver.findElements(By.cssSelector("[role=\"listbox\"]"));
+        for (WebElement dropDown: dropDownBoxes) {
+            dropDown.click();
+            dropDown.sendKeys(Keys.ENTER);
+        }
+
+        driver.findElement(By.cssSelector(".card button[role=button]")).click();
+        driver.findElement(By.cssSelector(".close-toastr")).click();
+        return itemName;
+    }
+}
+
+//Navigate to  https://pwa-woo.wpmobilepack.com/#/category/88
+//
+//Add two random items to the cart
+//    Get the name of the item
+//
+//Navigate to cart.
+//    Verify name of the item is displayed in the cart.
+//
+//Total of two items should be equal to number next to Total text
+//
+//Note:
+//  Some items have drop down you need to handle those drop down(s). Choose first option    from the dropdown.
+//   Do not use Thread.sleep
+//   Each item should be added one time.
+//   To make sure your code is working run it at least 5 times.
