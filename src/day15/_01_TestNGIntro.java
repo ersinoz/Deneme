573b7b51068c2528d2a96860017cf1ed9571427d
daulet2030 <daulet2030@gmail.com>
2020-07-10 20:34:03 +0600

    day 15 testNG example

---
+++ src/day15/_01_TestNGIntro.java
@@ -0,0 +1,18 @@
+package day15;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _01_TestNGIntro {
+
+    @Test
+    void testCase1() {
+        Assert.assertEquals("test", "test");
+    }
+
+    @Test
+    void testCase2() {
+
+    }
+}
