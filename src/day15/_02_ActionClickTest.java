92e7c85ab607950976668363913afcde7c77c246
daulet2030 <daulet2030@gmail.com>
2020-07-10 21:26:34 +0600

    day 15 reusing the same browser

--- src/day15/_02_ActionClickTest.java
+++ src/day15/_02_ActionClickTest.java
@@ -8,16 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class _02_ActionClickTest {
 
     private WebDriver driver;
     private Actions builder;
 
-    @BeforeMethod
+    @BeforeClass
     void setup(){
         System.setProperty("webdriver.chrome.driver", "E:\\projects\\Selenium\\driver\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -25,7 +23,7 @@ public class _02_ActionClickTest {
         builder = new Actions(driver);
     }
 
-    @AfterMethod
+    @AfterClass
     void closeDriver() {
         driver.quit();
     }
