dac4c14fb35ac7e65dadc3ac3b16a7121c55593e
daulet2030 <daulet2030@gmail.com>
2020-07-10 20:48:52 +0600

    day 15 testNG before and after methods

--- src/day15/_01_TestNGIntro.java
+++ src/day15/_01_TestNGIntro.java
@@ -3,6 +3,8 @@ package day15;
 
 import org.testng.Assert;
 import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Random;
@@ -11,6 +13,7 @@ public class _01_TestNGIntro {
 
     @Test
     void testCase1() {
+        System.out.println("Successful test case 1");
         Assert.assertEquals("test", "test");
     }
 
@@ -18,15 +21,26 @@ public class _01_TestNGIntro {
     void testCase2() {
         boolean failCondition = true;
         if(failCondition) {
-            Assert.fail("This test fail because of some condition!");
+            Assert.fail("This test case 2 fail because of some condition!");
         }
     }
 
     @Test
     void testCase3() {
+        System.out.println("Sometimes successful test case 3");
         boolean dataIsAvailable = new Random().nextBoolean();
         if(!dataIsAvailable) {
             throw new SkipException("Data required for test is not available!");
         }
+    }
+
+    @BeforeMethod
+    void setup() {
+        System.out.println("Initializing WebDriver!");
+    }
+
+    @AfterMethod
+    void cleanUp(){
+        System.out.println("Driver close!");
     }
 }
