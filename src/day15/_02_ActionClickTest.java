bf0e05463e3918911484d40a64fa7c557e1d5e8a
daulet2030 <daulet2030@gmail.com>
2020-07-10 21:22:39 +0600

    day 15 cleanup

--- src/day15/_02_ActionClickTest.java
+++ src/day15/_02_ActionClickTest.java
@@ -15,12 +15,14 @@ import org.testng.annotations.Test;
 public class _02_ActionClickTest {
 
     private WebDriver driver;
+    private Actions builder;
 
     @BeforeMethod
     void setup(){
         System.setProperty("webdriver.chrome.driver", "E:\\projects\\Selenium\\driver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://demoqa.com/buttons");
+        builder = new Actions(driver);
     }
 
     @AfterMethod
@@ -32,9 +34,7 @@ public class _02_ActionClickTest {
     void actionClickTestCase() {
         WebElement clickButton = driver.findElement(By.xpath("//button[text()='Click Me']"));
 
-        Actions builder = new Actions(driver);
-        Action build = builder.moveToElement(clickButton).click().build();
-        build.perform();
+        builder.moveToElement(clickButton).click().perform();
 
         String dynamicClickMessage = driver.findElement(By.id("dynamicClickMessage")).getText();
         Assert.assertEquals("You have done a dynamic click", dynamicClickMessage);
@@ -45,9 +45,7 @@ public class _02_ActionClickTest {
     void actionDoubleClickTestCase() {
         WebElement clickButton = driver.findElement(By.xpath("//button[text()='Double Click Me']"));
 
-        Actions builder = new Actions(driver);
-        Action build = builder.doubleClick(clickButton).build();
-        build.perform();
+        builder.doubleClick(clickButton).perform();
 
         String dynamicClickMessage = driver.findElement(By.id("doubleClickMessage")).getText();
         Assert.assertEquals("You have done a double click", dynamicClickMessage);
@@ -57,9 +55,7 @@ public class _02_ActionClickTest {
     void actionRightClickTest() {
         WebElement clickButton = driver.findElement(By.xpath("//button[text()='Right Click Me']"));
 
-        Actions builder = new Actions(driver);
-        Action build = builder.contextClick(clickButton).build();
-        build.perform();
+        builder.contextClick(clickButton).perform();
 
         String dynamicClickMessage = driver.findElement(By.id("rightClickMessage")).getText();
         Assert.assertEquals("You have done a right click", dynamicClickMessage);
