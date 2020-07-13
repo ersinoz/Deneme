9229a68b3e29b12d991b27f62e6c983da3ed23ce
daulet2030 <daulet2030@gmail.com>
2020-07-13 20:21:47 +0600

    day 16 login test case

--- src/day16/_01_DemoQA.java
+++ src/day16/_01_DemoQA.java
@@ -2,8 +2,6 @@ package day16;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,19 +9,25 @@ import utils.BaseDriver;
 
 public class _01_DemoQA extends BaseDriver {
 
-    private String email;
+    private String username;
     private String password;
 
     @BeforeClass
     void goToWebsite() {
-        email = "test12345asda@test.com";
-        password = "someRassword123";
+        username = "daulet";
+        password = "DV@d9FjCm";
         driver.get("https://demoqa.com/books");
     }
 
     @Test()
     void loginTestCase() {
-
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("userName")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#userName-value")));
+        Assert.assertEquals(username,driver.findElement(By.cssSelector("#userName-value")).getText());
     }
+
 
 }
--- src/utils/BaseDriver.java
+++ src/utils/BaseDriver.java
@@ -3,18 +3,21 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class BaseDriver {
     protected WebDriver driver;
     protected Actions builder;
+    protected WebDriverWait wait;
 
     @BeforeClass
     protected void setup(){
         System.setProperty("webdriver.chrome.driver", "E:\\projects\\Selenium\\driver\\chromedriver.exe");
         driver = new ChromeDriver();
         builder = new Actions(driver);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterClass
