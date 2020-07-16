9f1c2bf38c75ffadb1bbc070570472673f6e4fcf
daulet2030 <daulet2030@gmail.com>
2020-07-16 20:17:35 +0600

    day 18 two classes

---
+++ src/day18/_02_SimpleTestNGTests.java
@@ -0,0 +1,32 @@
+package day18;
+
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.*;
+
+import java.util.Random;
+
+public class _02_SimpleTestNGTests {
+
+    @Test
+    void testCase1() {
+        System.out.println("Successful test case 1");
+    }
+
+    @Test
+    void testCase2() {
+        System.out.println("Successful test case 2");
+    }
+
+    @Test
+    void testCase3() {
+        System.out.println("Successful test case 3");
+    }
+
+    @Test
+    void testCase4() {
+        System.out.println("Successful test case 4");
+    }
+
+}
