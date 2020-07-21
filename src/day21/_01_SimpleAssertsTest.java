a22648ed016e1347fb091312fe4138875d9b4ca9
daulet2030 <daulet2030@gmail.com>
2020-07-21 20:19:46 +0600

    day 21 simple asserts

--- src/day21/_01_SimpleAssertsTest.java
+++ src/day21/_01_SimpleAssertsTest.java
@@ -24,13 +24,27 @@ public class _01_SimpleAssertsTest {
     void testCase3() {
         Integer[] actual = {1, 2, 3, 4};
         Integer[] expected = {1, 4, 3, 2};
-        Assert.assertEqualsNoOrder(actual, expected, "Arrays are not equal");
+        Assert.assertEqualsNoOrder(actual, expected, "Arrays are equal");
     }
 
     @Test()
     void testCase4() {
         Integer[] actual = {1, 2, 3, 4};
         Integer[] expected = {1, 4, 3, 5};
-        Assert.assertEqualsNoOrder(actual, expected, "Arrays are not equal");
+        Assert.assertEqualsNoOrder(actual, expected, "Arrays are equal");
+    }
+
+    @Test()
+    void testCase5() {
+        double actual = 10.0;
+        double expected = 10.1;
+        Assert.assertEquals(actual, expected, 0.1);
+    }
+
+    @Test()
+    void testCase6() {
+        double actual = 10.1;
+        double expected = 10.0;
+        Assert.assertNotEquals(actual, expected, 0.1, "Expected not to be equal withing 0.1 delta!");
     }
 }
