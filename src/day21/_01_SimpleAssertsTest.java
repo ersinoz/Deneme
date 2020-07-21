2b851cd1bde7acbbb3089e7a390985316d539ddc
daulet2030 <daulet2030@gmail.com>
2020-07-21 20:13:13 +0600

    day 21 simple asserts optional message

--- src/day21/_01_SimpleAssertsTest.java
+++ src/day21/_01_SimpleAssertsTest.java
@@ -10,27 +10,27 @@ public class _01_SimpleAssertsTest {
     void testCase1() {
         Integer[] actual = {1, 2, 3, 4};
         Integer[] expected = {1, 2, 3, 4};
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(actual, expected, "Arrays are not equal by order");
     }
 
     @Test()
     void testCase2() {
         Integer[] actual = {1, 2, 3, 4};
         Integer[] expected = {1, 4, 3, 2};
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(actual, expected, "Arrays are not equal by order");
     }
 
     @Test()
     void testCase3() {
         Integer[] actual = {1, 2, 3, 4};
         Integer[] expected = {1, 4, 3, 2};
-        Assert.assertEqualsNoOrder(actual, expected);
+        Assert.assertEqualsNoOrder(actual, expected, "Arrays are not equal");
     }
 
     @Test()
     void testCase4() {
         Integer[] actual = {1, 2, 3, 4};
         Integer[] expected = {1, 4, 3, 5};
-        Assert.assertEqualsNoOrder(actual, expected);
+        Assert.assertEqualsNoOrder(actual, expected, "Arrays are not equal");
     }
 }
