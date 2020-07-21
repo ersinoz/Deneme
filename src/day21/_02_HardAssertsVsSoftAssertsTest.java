cf56249ab31e4264f212a675b96aacb05b6d8e78
daulet2030 <daulet2030@gmail.com>
2020-07-21 20:48:19 +0600

    day 21 soft Assert

--- src/day21/_02_HardAssertsVsSoftAssertsTest.java
+++ src/day21/_02_HardAssertsVsSoftAssertsTest.java
@@ -2,6 +2,7 @@ package day21;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class _02_HardAssertsVsSoftAssertsTest {
 
@@ -25,5 +26,35 @@ public class _02_HardAssertsVsSoftAssertsTest {
         System.out.println("After Hard Assert!");
     }
 
+    @Test()
+    void softAssert() {
+        System.out.println("Before Soft Assert!");
+        SoftAssert softAssert = new SoftAssert();
+        Integer[] actual = {1, 2, 3, 4};
+        Integer[] expected = {1, 2, 3, 4};
+        softAssert.assertEquals(actual, expected, "Arrays are not equal by order");
+        softAssert.assertEquals(actual, expected, "Arrays are not equal by order");
+        softAssert.assertEquals(actual, expected, "Arrays are not equal by order");
 
+        System.out.println("After Soft Assert!");
+        softAssert.assertAll();
+        System.out.println("After Assert All!");
+    }
+
+    @Test()
+    void softAssertFailure() {
+        System.out.println("Before Soft Assert!");
+        SoftAssert softAssert = new SoftAssert();
+        Integer[] actual = {1, 2, 3, 4};
+        Integer[] expected = {1, 2, 3, 5};
+        softAssert.assertEquals(actual, expected, "Arrays are not equal by order");
+        softAssert.assertEquals(actual, expected, "Arrays are not equal by order");
+        softAssert.assertEquals(actual, expected, "Arrays are not equal by order");
+        softAssert.assertNotEquals(actual, expected, "Arrays are not equal by order");
+
+        System.out.println("After Soft Assert!");
+        softAssert.assertAll();
+        System.out.println("After Assert All!");
+
+    }
 }
