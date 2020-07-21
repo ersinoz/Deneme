84c35ae62bfae7623f0c1aec55a94cfaca75a19a
daulet2030 <daulet2030@gmail.com>
2020-07-21 20:11:19 +0600

    day 21 simple asserts

---
+++ src/day21/_01_SimpleAssertsTest.java
@@ -0,0 +1,36 @@
+package day21;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class _01_SimpleAssertsTest {
+
+    @Test()
+    void testCase1() {
+        Integer[] actual = {1, 2, 3, 4};
+        Integer[] expected = {1, 2, 3, 4};
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test()
+    void testCase2() {
+        Integer[] actual = {1, 2, 3, 4};
+        Integer[] expected = {1, 4, 3, 2};
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test()
+    void testCase3() {
+        Integer[] actual = {1, 2, 3, 4};
+        Integer[] expected = {1, 4, 3, 2};
+        Assert.assertEqualsNoOrder(actual, expected);
+    }
+
+    @Test()
+    void testCase4() {
+        Integer[] actual = {1, 2, 3, 4};
+        Integer[] expected = {1, 4, 3, 5};
+        Assert.assertEqualsNoOrder(actual, expected);
+    }
+}
